4c3f8f1083ef543b989953996c37bc2a1b9acbdf
Alexander Nieves <93687414+alexandernieves@users.noreply.github.com>
2025-06-20 00:26:01 -0400

    Updated test-mongo.js

--- backend/test-mongo.js
+++ backend/test-mongo.js
@@ -1,6 +1,6 @@
+const mongoose = require('mongoose'); // 🔥 IMPORTANTE
+
 const uri = "mongodb://alexcode:alexcodeworkspace@cluster0-shard-00-00.kqwhh3k.mongodb.net:27017,cluster0-shard-00-01.kqwhh3k.mongodb.net:27017,cluster0-shard-00-02.kqwhh3k.mongodb.net:27017/admin?ssl=true&replicaSet=atlas-v11nkj-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-
 
 (async () => {
   try {
