f9e2da00c9409f6e8f8a1580b28a8879674397e8
Alexander Nieves <93687414+alexandernieves@users.noreply.github.com>
2025-06-20 00:25:19 -0400

    Updated test-mongo.js

--- backend/test-mongo.js
+++ backend/test-mongo.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose'); // <-- ESTO FALTABA
+const uri = "mongodb://alexcode:alexcodeworkspace@cluster0-shard-00-00.kqwhh3k.mongodb.net:27017,cluster0-shard-00-01.kqwhh3k.mongodb.net:27017,cluster0-shard-00-02.kqwhh3k.mongodb.net:27017/admin?ssl=true&replicaSet=atlas-v11nkj-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://alexcode:alexcodeworkspace@cluster0.kqwhh3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 (async () => {
   try {
