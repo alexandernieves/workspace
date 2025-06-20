95d3c0f0a33d72adacbaf33c742b8c4e38be7d0f
Alexander Nieves <93687414+alexandernieves@users.noreply.github.com>
2025-06-20 00:21:52 -0400

    Updated test-mongo.js

---
+++ backend/test-mongo.js
@@ -0,0 +1,14 @@
+// test-mongo.js
+import mongoose from 'mongoose';
+
+const uri = "mongodb+srv://alexcode:alexcodeworkspace@cluster0.kqwhh3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+(async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("✅ Conexión exitosa a MongoDB");
+    await mongoose.connection.close();
+  } catch (error) {
+    console.error("❌ Error al conectar a MongoDB:", error.message);
+  }
+})();
